727d85b920f36eb20f863d54ad67bad83ab2a6c6
cscosine-dev <cscosine@gmail.com>
2026-05-29 12:16:47 +0200

    add open code sh to help

---
+++ open-code-tmp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# assume you have already csCMake, which in turns activate the csOrchestrator venv
+# TODO remove when pip available for csOrchestrator
+source workspace/csCMake/.venv/bin/activate
+
+code .
