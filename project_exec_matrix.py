be09c66c33185319061d1fec4973ff24c027284f
cscosine-dev <cscosine@gmail.com>
2026-05-30 12:16:29 +0200

    sign of life of steps

--- project_exec_matrix.py
+++ project_exec_matrix.py
@@ -72,8 +72,8 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
 
     p = o.create_phase("Repos Update")
 
-    skip_get_repository = True
-    skip_build = True
+    skip_get_repository = False
+    skip_build = False
 
     if skip_get_repository:
         report.append_warning("Skipping repository cloning steps")
