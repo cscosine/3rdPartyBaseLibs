a3f01b4204344d0b72d54a4eee6dedbf60dec5ff
cscosine-dev <cscosine@gmail.com>
2026-06-01 03:00:49 +0200

    first test compile ?!?

--- project_exec_matrix.py
+++ project_exec_matrix.py
@@ -69,7 +69,7 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
     p = o.create_phase("Repos Update")
 
     skip_get_repository = False
-    skip_build = True
+    skip_build = False
 
     if skip_get_repository:
         report.append_warning("Skipping repository cloning steps")
