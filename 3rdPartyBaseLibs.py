6df24a897e49416ce1df95321b1c1749a40be416
cscosine-dev <cscosine@gmail.com>
2026-07-29 12:46:10 +0200

    remove env variables from create archives step

--- 3rdPartyBaseLibs.py
+++ 3rdPartyBaseLibs.py
@@ -57,17 +57,17 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
     repos: dict[str, None | BuildConfig] = {
         "csCMake": None,
         "eigen3": BuildConfig.RELEASE,
-        "fmt": BuildConfig.DEBUG_RELEASE,
-        "fmt-eigen": BuildConfig.RELEASE,
-        "cpptrace": BuildConfig.DEBUG_RELEASE,
-        "magic_enum": BuildConfig.DEBUG_RELEASE,
-        "libassert": BuildConfig.DEBUG_RELEASE,
-        "tclap": BuildConfig.RELEASE,
-        "Catch2": BuildConfig.DEBUG_RELEASE,
-        "pipes": BuildConfig.RELEASE,
-        "NamedType": BuildConfig.RELEASE,
-        "tl-optional": BuildConfig.RELEASE,
-        "tl-expected": BuildConfig.RELEASE,
+        # "fmt": BuildConfig.DEBUG_RELEASE,
+        # "fmt-eigen": BuildConfig.RELEASE,
+        # "cpptrace": BuildConfig.DEBUG_RELEASE,
+        # "magic_enum": BuildConfig.DEBUG_RELEASE,
+        # "libassert": BuildConfig.DEBUG_RELEASE,
+        # "tclap": BuildConfig.RELEASE,
+        # "Catch2": BuildConfig.DEBUG_RELEASE,
+        # "pipes": BuildConfig.RELEASE,
+        # "NamedType": BuildConfig.RELEASE,
+        # "tl-optional": BuildConfig.RELEASE,
+        # "tl-expected": BuildConfig.RELEASE,
     }
 
     o = create_orchestrator_factory_all_supported_cases(
@@ -147,7 +147,7 @@ def create_orchestrator() -> OptionalOrchestratorWithReport:
             input_id="versions",
             input_dict="packages",
             base_install_dir=base_install_dir,
-        ).add_extra(StepSkipExecutionOnLocal())
+        )#.add_extra(StepSkipExecutionOnLocal())
     )
 
     p.add_step(
